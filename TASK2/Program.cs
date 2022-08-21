a1ce860306865f5cca53a984256cdc72d66c4ba5
Ilya Yurchenkov <yurchenkov.99@gmail.com>
2022-08-21 21:59:14 +0300

    TASK_2

---
+++ TASK2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+Console.Clear();
+int size = ReadInt("Введите размер массива: ");
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (i % 2 != 0)
+    sum = sum + numbers [i];
+    else
+    i = i++;
+}
+
+Console.WriteLine($"Сумма чисел на нечетных позициях массива {sum}");
+
+void FillArrayRandomNumbers(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(1, 10);
+}
+}
+
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
